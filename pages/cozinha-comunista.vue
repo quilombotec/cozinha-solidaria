e629675728c4992cfd97e52e4b3113eb54f7d186
Cândido da Silva Junior <57716228+candidosilva@users.noreply.github.com>
2025-03-05 11:57:20 -0300

    Adicionado página por que cozinhas comunistas

---
+++ pages/cozinha-comunista.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-container style="font-family: monospace">
+    <h1 class="mb-4">Por que cozinhas comunistas ?</h1>
+    <div class="ga-4 d-flex flex-column">
+      <p>
+        Após qualquer catástrofe como enchentes e terremotos os humanos se
+        juntam para ajudar de graça os afetados pelo desastre, seja doando
+        alimento, roupas ou tempo para construir o que foi destruído. Isso por
+        que os humanos são comunistas por natureza e ao ver o desespero e a dor
+        voltamos para a nossa essência e só agimos de forma contrária no nosso
+        dia a dia por que somos ensinados desde de criança pela ideologia do
+        capitalismo a sermos egoístas.
+      </p>
+
+      <div>
+
+      </div>
+      <p>
+        Na escola somos ensinados a competir com os colegas por uma vaga na
+        universidade em vez de cooperar para criarmos uma sociedade onde todos
+        possam ter acesso a educação gratuita.
+        <p>
+
+            Na universidade somos ensinados a
+            competir por uma vaga no mercado de trabalho em vez de cooperar para
+            criarmos uma sociedade onde todos possamos ter trabalho.
+        </p>
+        <p>
+
+            No trabalho
+           temos que competir pela promoção em vez de cooperar para que todos
+           possamos ter trabalho digno. 
+        </p>
+
+        <p>
+
+            As empresas no capitalismo competem por
+            mais dinheiro em vez de cooperar umas com as outras para benefício da
+            sociedade.
+        </p>
+        <p>
+
+            A religião no capitalismo ensina que só aquele que faz parte
+           dela será salvo enquanto outros irão para uma condenação eterna.
+        </p>
+      </p>
+      <p>
+        A todo momento somos ensinados a negar nossa solidariedade,
+        cooperatividade, empatia e humanidade em nome do lucro dos bilionários enquanto os
+        trabalhadores em sua maioria tem dificuldade de ter o básico. A comida é cara e com agrotóxicos que nos adoecem. 
+        Não temos acesso a saúde com
+        planos de saúde que custam um valor exorbitante. Não temos acesso a moradia digna com muitos morando na rua ou
+        pagando um valor extenuante de aluguel ou financiamento. Não temos
+        educação de qualidade e novamente poucos podem pagar por colégios ou
+        universidades caríssimas enquanto a educação pública é precária. Não
+        temos acesso a lazer com ingressos para shows, cinemas, festas ou
+        partidas de futebol caríssimos. No transporte público, que é gerenciado por empresas privadas, somos tratados pior que gado indo para o abate confinados em espaços lotados e de um calor insuportável. Tudo isso em nome do lucro de
+        bilionários.
+      </p>
+
+      <p>Tudo no capitalismo é para beneficiar os bilionários.</p>
+
+      <div>
+        <p>Mas não nas cozinhas comunistas pois,</p>
+        <p>Aqui a vida humana é mais importante</p>
+        <p>Aqui a cooperação é mais importante</p>
+        <p>Aqui a empatia é mais importante</p>
+        <p>Aqui onde o alimento é gratuíto</p>
+        <p>Nós voltamos a ser humanos</p>
+        <p>Voltamos a ser comunistas.</p>
+      </div>
+    </div>
+  </v-container>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "semLayout",
+});
+</script>
