e4461dc98992471d0ad8da900c0f53d31e1b67a9
Cândido da Silva Junior <57716228+candidosilva@users.noreply.github.com>
2025-03-09 11:04:30 -0300

    Acesso ao texto cozinha comunista

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -1,6 +1,11 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const usuarioStore = useUsuarioStore();
-  if (to.path === "/login" || to.path === "/cadastro") return;
+  if (
+    to.path === "/login" ||
+    to.path === "/cadastro" ||
+    to.path === "/cozinha-comunista"
+  )
+    return;
 
   if (usuarioStore.autenticado === false) {
     return navigateTo("/login");
